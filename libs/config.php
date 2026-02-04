448a4aa62780387db92221f54737bb1b1a13ea8a
moralesbarbamanuel-svg <moralesbarbamanuel@gmail.com>
2026-02-04 01:38:06 +0100

    Update config.php

--- libs/config.php
+++ libs/config.php
@@ -1,12 +1,9 @@
 <?php
 // Configuración de conexión a la base de datos
 // Ajusta estos valores según tu entorno XAMPP/MySQL
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'dwes');
+define('DB_HOST', 'metro.proxy.rlwy.net:54578/railway');
+define('DB_NAME', 'railway');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-
-// Charset opcional
-define('DB_CHARSET', 'utf8');
-
+define('DB_PASS', 'iCqVrubLjyyoKCvHlaSJqSdingwTPLlS');
+//mysql://root:iCqVrubLjyyoKCvHlaSJqSdingwTPLlS@metro.proxy.rlwy.net:54578/railway
 ?>
